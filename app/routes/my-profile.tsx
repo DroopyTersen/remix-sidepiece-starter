7d340b681ecdd6b51a784fe7b122402816ff08dc
Andrew Petersen <andrew.petersen15@gmail.com>
2022-11-05 13:26:58 -0600

    finished my profile display styling

--- app/routes/my-profile.tsx
+++ app/routes/my-profile.tsx
@@ -1,5 +1,6 @@
 import { json, LoaderArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { useState } from "react";
 import { requireAuthenticatedLoader } from "~/features/auth/auth.remix.server";
 import { MainContentPadded } from "~/features/layout/AppLayout";
 import { AppErrorBoundary } from "~/toolkit/components/errors/AppErrorBoundary";
@@ -13,19 +14,46 @@ export const loader = async ({ request, params }: LoaderArgs) => {
 
 export default function MyProfileRoute() {
   let data = useLoaderData<typeof loader>();
+  let [photo, setPhoto] = useState(
+    data.user.photo || "https://via.placeholder.com/300"
+  );
+  let [name, setName] = useState(data.user.name || "");
   return (
     <MainContentPadded>
-      <h1 className="text-secondary">My Profile</h1>
-      <fieldset>
-        <InputField label="Name" name="name" defaultValue={data.user.name} />
-        <InputField
-          label="Username"
-          name="username"
-          disabled
-          defaultValue={data.user.username}
+      <div className="flex items-center gap-4">
+        <img
+          alt="Profile photo"
+          className="w-24 m-0 rounded-full"
+          src={photo || "https://via.placeholder.com/160?text=No Photo"}
         />
-        <InputField label="Photo" name="photo" defaultValue={data.user.photo} />
-      </fieldset>
+        <div>
+          <div className="text-lg">My Profile</div>
+          <h1 className="m-0 text-secondary">{name}</h1>
+        </div>
+      </div>
+      <form className="mt-4">
+        <fieldset className="space-y-4">
+          <InputField
+            label="Name"
+            name="name"
+            defaultValue={data.user.name}
+            required
+            onBlur={(e) => setName(e.currentTarget.value)}
+          />
+          <InputField
+            label="Username"
+            name="username"
+            disabled
+            defaultValue={data.user.username}
+          />
+          <InputField
+            label="Photo"
+            name="photo"
+            defaultValue={data.user.photo}
+            onBlur={(e) => setPhoto(e.currentTarget.value)}
+          />
+        </fieldset>
+      </form>
     </MainContentPadded>
   );
 }
